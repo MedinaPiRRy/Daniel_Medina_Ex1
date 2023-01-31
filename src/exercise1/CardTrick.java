8b9a770ee0cf70a0f1039aea04627e490a4d63c9
Daniel_Medina <Medinald@sheridancollege.ca>
2023-01-31 17:59:43 -0500

    Added class

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,6 @@
-package exercise1;
 
+package exercise1;
+import java.util.Scanner;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -11,11 +12,17 @@ package exercise1;
 public class CardTrick {
     
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         
         Card[] hand = new Card[7];
-
+        Card card = new Card();
+        
         for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
+            int a = (int) ((Math.random() * 13) + 1);
+            card.setValue(a);
+            
+            int b = (int) (Math.random() * 3);
+            card.setSuit(card.SUITS[b]);
             //card.setValue(insert call to random number generator here)
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
@@ -23,8 +30,27 @@ public class CardTrick {
             //       Don't worry about duplicates at this point
         }
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
+        System.out.println("Choose your card");
+        System.out.println("Choose the value of your card");
+        System.out.println("Remember that 1: ace, 11: jack, 12: queen, 13: king");
+        Card user = new Card();
+        int a1 = input.nextInt();
+        user.setValue(a1);
+        System.out.println("Choose yout card's suit");
+        System.out.println("0: Hearts, 1: Diamonds, 2: Spades, 3: Clubs");
+        int a2 = input.nextInt();
+        user.setSuit(user.SUITS[a2]);
+        for (int i = 0; i < hand.length; i++) {
+            if (hand[i].getSuit().equals(user.getSuit()) && hand[i].getValue() == user.getValue()) {
+            printInfo();
+        } else  {
+            System.out.println("There is no matches");
+        }
+        
+        }
+        
+        printInfo();
+        
         // Hint: You can ask for values 1 to 10, and then
         //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
         //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
@@ -45,19 +71,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Daniel Medina");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Get into the IT team in my job");
+        System.out.println("-- Finish school as soon as possible");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Reading");
+        System.out.println("-- Watching movies");
+        System.out.println("-- Listening to music");
+        System.out.println("-- Create and play music");
 
         System.out.println();
         
